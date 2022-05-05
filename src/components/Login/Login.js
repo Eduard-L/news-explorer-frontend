cfc30b7315ca95c62af252ca5211905d3d218747
Eduard-L <lwqtbdwrd@gmail.com>
2022-05-05 10:54:21 +0300

    add:authForm

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import { PopupWithForm } from "../PopupWithForm/PopupWithForm";
 import { useForm } from '../../formHooks/useForm'
+import { AuthForm } from "../AuthForm/AuthForm";
 
 export function Login({ btnText, onSubmit, redirectText, onClick, isOpen, setEmail, setPassword, globalErrorMessage, isFormLoading, isSignUpOpen, isSignInOpen, onClose, preSpanText }) {
 
@@ -7,14 +8,7 @@ export function Login({ btnText, onSubmit, redirectText, onClick, isOpen, setEma
 
   return (
     <PopupWithForm btnText={btnText} isFormLoading={isFormLoading} onSubmit={onSubmit} resetForm={resetForm} isValid={isValid} onClose={onClose} redirectText={redirectText} isSignUpOpen={isSignUpOpen} isOpen={isOpen} preSpanText={preSpanText} onClick={onClick} type={isSignUpOpen ? 'signup' : 'signin'} text={isSignInOpen ? 'Sign In' : 'Sign up'} >
-      <label className='popup__label' htmlFor='email'>Email</label>
-      <input name='email' value={values.email || ''} onChange={(e) => { setEmail(e.target.value); handleChange(e); }} autoComplete="email" className='popup__input' type='email' id='email' placeholder='Enter email' required />
-      <span id="input_type_email-error" className="popup__error">{errors.email ? errors.email : ''}</span>
-      <label className='popup__label' htmlFor='password'>password</label>
-      <input name="password" value={values.password || ''} onChange={(e) => { setPassword(e.target.value); handleChange(e); }} autoComplete="password" className='popup__input' type='password' id='password' placeholder='Enter password' required minLength={6} />
-      <span id="input_type_password-error" className="popup__error">{errors.password ? errors.password : ''}</span>
-      <span id="input_type_global-error" className="popup__error">{globalErrorMessage && globalErrorMessage}</span>
-
+      <AuthForm setEmail={setEmail} handleChange={handleChange} errors={errors} values={values} setPassword={setPassword} globalErrorMessage={globalErrorMessage} />
     </PopupWithForm>
   )
 }
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,20 +1,17 @@
 
 import { PopupWithForm } from "../PopupWithForm/PopupWithForm";
 import { useForm } from "../../formHooks/useForm";
+import { AuthForm } from "../AuthForm/AuthForm";
 export function Registration({ setName, btnText, onSubmit, redirectText, onClick, isOpen, setEmail, setPassword, globalErrorMessage, isFormLoading, isSignUpOpen, isSignInOpen, onClose, preSpanText }) {
   const { handleChange, values, isValid, errors, resetForm } = useForm();
   return (
     <PopupWithForm btnText={btnText} isFormLoading={isFormLoading} onSubmit={onSubmit} resetForm={resetForm} isValid={isValid} onClose={onClose} redirectText={redirectText} isSignUpOpen={isSignUpOpen} isOpen={isOpen} preSpanText={preSpanText} onClick={onClick} type={isSignUpOpen ? 'signup' : 'signin'} text={isSignInOpen ? 'Sign In' : 'Sign up'}>
-      <label className='popup__label' htmlFor='email'>Email</label>
-      <input name='email' value={values.email || ''} onChange={(e) => { setEmail(e.target.value); handleChange(e); }} autoComplete="email" className='popup__input' type='email' id='email' placeholder='Enter email' required />
-      <span id="input_type_email-error" className="popup__error">{errors.email ? errors.email : ''}</span>
-      <label className='popup__label' htmlFor='password'>password</label>
-      <input name="password" value={values.password || ''} onChange={(e) => { setPassword(e.target.value); handleChange(e); }} autoComplete="password" className='popup__input' type='password' id='password' placeholder='Enter password' required minLength={6} />
-      <span id="input_type_password-error" className="popup__error">{errors.password ? errors.password : ''}</span>
-      <label className='popup__label' htmlFor='username'>Username</label>
-      <input name='name' value={values.name || ''} onChange={(e) => { setName(e.target.value); handleChange(e); }} autoComplete='username' className='popup__input' type='text' id='username' placeholder='Enter your username' required minLength={2} />
-      <span id="input_type_username-error" className="popup__error">{errors.name ? errors.name : ''}</span>
-      <span id="input_type_global-error" className="popup__error">{globalErrorMessage && globalErrorMessage}</span>
+      <AuthForm setEmail={setEmail} handleChange={handleChange} errors={errors} values={values} setPassword={setPassword} globalErrorMessage={globalErrorMessage}>
+        <label className='popup__label' htmlFor='username'>Username</label>
+        <input name='name' value={values.name || ''} onChange={(e) => { setName(e.target.value); handleChange(e); }} autoComplete='username' className='popup__input' type='text' id='username' placeholder='Enter your username' required minLength={2} />
+        <span id="input_type_username-error" className="popup__error">{errors.name ? errors.name : ''}</span>
+      </AuthForm>
+
     </PopupWithForm>
   )
 }
